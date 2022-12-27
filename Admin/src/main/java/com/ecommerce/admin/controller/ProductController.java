0a93aee3a89c5d064d92a1c05c91941542e0f561
uk47kumar <uk47kumar@gmail.com>
2022-12-27 23:25:23 +0530

    product and image add to db

--- Admin/src/main/java/com/ecommerce/admin/controller/ProductController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/ProductController.java
@@ -8,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.util.List;
@@ -34,11 +39,28 @@ public class ProductController {
     }
 
     @GetMapping("/add-product")
-    public String addProductForm(Model model){
+    public String addProductForm(Model model, Principal principal){
+        if(principal == null){
+            return "redirect:/login";
+        }
         List<Category> categories = categoryService.findAllByActivated();
+        model.addAttribute("title","Add Product");
         model.addAttribute("categories",categories);
         model.addAttribute("product",new ProductDto());
-        model.addAttribute("title","Add Product");
         return "add-product";
+    }
+
+    @PostMapping("/save-product")
+    public String saveProduct(@ModelAttribute("product")ProductDto productDto,
+                              @RequestParam("imageProduct") MultipartFile imageProduct,
+                              RedirectAttributes attributes){
+        try {
+            productService.save(imageProduct,productDto);
+            attributes.addFlashAttribute("success","Added Successfully");
+        }catch (Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("error","Failed to Add");
+        }
+        return "redirect:/products";
     }
 }
--- Library/src/main/java/com/ecommerce/library/service/impl/ProductServiceImpl.java
+++ Library/src/main/java/com/ecommerce/library/service/impl/ProductServiceImpl.java
@@ -4,16 +4,23 @@ import com.ecommerce.library.dto.ProductDto;
 import com.ecommerce.library.model.Product;
 import com.ecommerce.library.repository.ProductRepository;
 import com.ecommerce.library.service.ProductService;
+import com.ecommerce.library.utils.ImageUpload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private ImageUpload imageUpload;
 
     @Override
     public List<ProductDto> findAll() {
@@ -31,13 +38,35 @@ public class ProductServiceImpl implements ProductService {
             productDto.setCategory(product.getCategory());
             productDto.setDeleted(product.getCategory().is_deleted());
             productDto.setActivated(product.is_activated());
+            productDtoList.add(productDto);
         }
         return productDtoList;
     }
 
     @Override
-    public Product save(ProductDto productDto) {
-        return null;
+    public Product save(MultipartFile imageProduct, ProductDto productDto){
+        try {
+            Product product = new Product();
+            if(imageProduct == null){
+                product.setImage(null);
+            }else {
+                if(imageUpload.uploadImage(imageProduct)){
+                    System.out.println("UPLOAD SUCCESSFULLY");
+                }
+                product.setImage(Base64.getEncoder().encodeToString(imageProduct.getBytes()));
+            }
+            product.setName(productDto.getName());
+            product.setDescription(productDto.getDescription());
+            product.setCategory(productDto.getCategory());
+            product.setCostPrice(productDto.getCostPrice());
+            product.setCurrentQuantity(productDto.getCurrentQuantity());
+            product.set_activated(true);
+            product.set_deleted(false);
+            return productRepository.save(product);
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
